876e47f8d23ef356f82cdfd8e2224cb65c5697fe
Issac In <66139562+issac-in@users.noreply.github.com>
2024-08-04 18:01:14 -0700

    Write a constructor for making "Book" objects
    To ensure the function is only called successfully with a constructor
    invocation, I added an initial check for the 'new' keyword.

--- logic.js
+++ logic.js
@@ -1,1 +1,19 @@
 "use strict"
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("Must use the 'new' keyword to call the constructor.");
+    }
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        const initialMsg =
+            `${this.title} by ${this.author}, ${this.pages} pages, `;
+        return read ?
+            initialMsg.concat("has been read.") :
+            initialMsg.concat("not read yet.");
+    };
+}
