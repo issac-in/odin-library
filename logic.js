e6c1f53b6f921d78ccab56dba8e54f1c06cb739c
Issac In <66139562+issac-in@users.noreply.github.com>
2024-08-07 19:51:15 -0700

    Enable the programmatic creation of catalog cards

--- logic.js
+++ logic.js
@@ -1,19 +1,272 @@
 "use strict"
 
-function Book(title, author, pages, read) {
+const UNREAD = "unread";
+const READING = "reading";
+const COMPLETED = "completed";
+
+const myCatalog = [
+    {
+        title: "A Dirty War: A Russian Reporter in Chechnya",
+        author: "Anna Politkovskaya",
+        status: "Completed",
+        isbn13: 9781860468971,
+        yearOfPublication: 2001,
+        summary: `The Chechen War was supposed to be over in 1996 after the first Yeltsin campaign, but in the summer of 1999, the new Putin government decided, in their own words, to "do the job properly". Before all the bodies of those who had died in the first campaign had been located or identified, many more thousands would be slaughtered in another round of fighting.\n\tThe first account to be written by a Russian woman, A Dirty War is an edgy and intense study of a conflict that shows no sign of being resolved. Exasperated by the Russian government's attempt to manipulate media coverage of the war, journalist Anna Politkovskaya undertook to go to Chechnya, to make regular reports and keep events in the public eye.\n\tIn a series of despatches from July 1999 to January 2001 she vividly describes the atrocities and abuses of war, whether it be the corruption endemic in post-Communist Russia, in particular the government and the military, or the spurious arguments and abominable behaviour of the Chechen authorities. In these courageous reports, Politkovskaya excoriates male stupidity and brutality on both sides of the conflict and interviews the civilians whose homes and communities have been laid waste, leaving them nowhere to live, and nothing and no one to believe in.`,
+        thumbnail: "./assets/thumbnails/width-160/160-a-dirty-war.avif",
+    },
+    {
+        title: "A Small Corner of Hell: Dispatches from Chechnya",
+        author: "Anna Politkovskaya",
+        status: "Reading",
+        isbn13: 9780226674339,
+        yearOfPublication: 2002,
+        summary: `Chechnya, a 6,000-square-mile corner of the northern Caucasus, has struggled under Russian domination for centuries. The region declared its independence in 1991, leading to a brutal war, Russian withdrawal, and subsequent "governance" by bandits and warlords. A series of apartment building attacks in Moscow in 1999, allegedly orchestrated by a rebel faction, reignited the war, which continues to rage today. Russia has gone to great lengths to keep journalists from reporting on the conflict; consequently, few people outside the region understand its scale and the atrocities—described by eyewitnesses as comparable to those discovered in Bosnia—committed there.\n\tAnna Politkovskaya, a correspondent for the liberal Moscow newspaper Novaya gazeta, was the only journalist to have constant access to the region. Her international stature and reputation for honesty among the Chechens allowed her to continue to report to the world the brutal tactics of Russia's leaders used to quell the uprisings. A Small Corner of Hell: Dispatches from Chechnya is her second book on this bloody and prolonged war. More than a collection of articles and columns, A Small Corner of Hell offers a rare insider's view of life in Chechnya over the past years. Centered on stories of those caught-literally-in the crossfire of the conflict, her book recounts the horrors of living in the midst of the war, examines how the war has affected Russian society, and takes a hard look at how people on both sides are profiting from it, from the guards who accept bribes from Chechens out after curfew to the United Nations. Politkovskaya's unflinching honesty and her courage in speaking truth to power combine here to produce a powerful account of what is acknowledged as one of the most dangerous and least understood conflicts on the planet. Anna Politkovskaya was assassinated in Moscow on October 7, 2006. "The murder of the journalist Anna Politkovskaya leaves a terrible silence in Russia and an information void about a dark realm that we need to know more about. No one else reported as she did on the Russian north Caucasus and the abuse of human rights there. Her reports made for difficult reading—and Politkovskaya only got where she did by being one of life's difficult people."—Thomas de Waal`,
+        thumbnail: "./assets/thumbnails/width-160/160-a-small-corner-of-hell.avif",
+    },
+    {
+        title: "Putin's Russia: Life in a Failing Democracy",
+        author: "Anna Politkovskaya",
+        status: "Unread",
+        isbn13: 9781429939157,
+        yearOfPublication: 2003,
+        summary: `A searing portrait of a country in disarray and of the man at its helm, from "the bravest of Russian journalists" (The New York Times)\n\tHailed as "a lone voice crying out in a moral wilderness" (New Statesman), Anna Politkovskaya made her name with her fearless reporting on the war in Chechnya. Here, she turned her steely gaze on the multiple threats to Russian stability, among them Vladimir Putin himself.\n\tRich with characters and poignant accounts, Putin's Russia depicts a far-reaching state of decay. Politkovskaya describes an army in which soldiers die from malnutrition, parents must pay bribes to recover their dead sons' bodies, and conscripts are even hired out as slaves. She exposes rampant corruption in business, government, and the judiciary, where everything from store permits to bus routes to court appointments is for sale. And she offers a scathing condemnation of the war in Chechnya, where kidnappings, extra-judicial killings, rape, and torture beget terrorism rather than fighting it. Finally, Politkovskaya denounces both Putin, for stifling civil liberties as he pushes the country back to a Soviet-style dictatorship, and the West, for its unqualified embrace of the Russian leader.\n\tSounding an urgent alarm, Putin's Russia is a gripping portrayal of a country in crisis and the testament of a great and intrepid reporter, who received death threats and survived assassination attempts for her scathing criticism of the Kremlin. Tragically, on October 7, 2006, Politkovskaya was shot and found dead in an elevator in her Moscow apartment building. After several years of investigations, five men were imprisoned for her murder.`,
+        thumbnail: "./assets/thumbnails/width-160/160-putin's-russia.avif.avif",
+    },
+    {
+        title: "A Russian Diary: A Journalist's Final Account of Life, Corruption, and Death in Putin's Russia",
+        author: "Anna Politkovskaya",
+        status: "Unread",
+        isbn13: 9780307497635,
+        yearOfPublication: 2007,
+        summary: `Anna Politkovskaya, one of Russia's most fearless journalists, was gunned down in a contract killing in Moscow in the fall of 2006. Just before her death, Politkovskaya completed this searing, intimate record of life in Russia from the parliamentary elections of December 2003 to the grim summer of 2005, when the nation was still reeling from the horrors of the Beslan school siege. In A Russian Diary, Politkovskaya dares to tell the truth about the devastation of Russia under Vladimir Putin-a truth all the more urgent since her tragic death.\n\tWriting with unflinching clarity, Politkovskaya depicts a society strangled by cynicism and corruption. As the Russian elections draw near, Politkovskaya describes how Putin neutralizes or jails his opponents, muzzles the press, shamelessly lies to the public-and then secures a sham landslide that plunges the populace into mass depression. In Moscow, oligarchs blow thousands of rubles on nights of partying while Russian soldiers freeze to death. Terrorist attacks become almost commonplace events. Basic freedoms dwindle daily.\n\tAnd then, in September 2004, armed terrorists take more than twelve hundred hostages in the Beslan school, and a different kind of madness descends.\n\tIn prose incandescent with outrage, Politkovskaya captures both the horror and the absurdity of life in Putin's Russia: She fearlessly interviews a deranged Chechen warlord in his fortified lair. She records the numb grief of a mother who lost a child in the Beslan siege and yet clings to the delusion that her son will return home someday. The staggering ostentation of the new rich, the glimmer of hope that comes with the organization of the Party of Soldiers' Mothers, the mounting police brutality, the fathomless public apathy-all are woven into Politkovskaya's devastating portrait of Russia today.\n\tIf anybody thinks they can take comfort from the 'optimistic' forecast, let them do so, Politkovskaya writes. It is certainly the easier way, but it is also a death sentence for our grandchildren.\n\tA Russian Diary is testament to Politkovskaya's ferocious refusal to take the easier way-and the terrible price she paid for it. It is a brilliant, uncompromising expose of a deteriorating society by one of the world's bravest writers.`,
+        thumbnail: "./assets/thumbnails/width-160/160-a-russian-diary.avif",
+    },
+];
+
+/**
+ * WARN: if thumbnail aka srcPath gets passed in as a "", that is NOT good.
+ * Note: Undecided if isbn13 and yearOfPublication should be String or Number
+ */
+function Book(title, author, status, isbn13, yearOfPublication, summary, thumbnail) {
     if (!new.target) {
         throw Error("Must use the 'new' keyword to call the constructor.");
     }
 
     this.title = title;
     this.author = author;
-    this.pages = pages;
-    this.read = read;
+    this.status = status;
+    this.isbn13 = isbn13;
+    this.yearOfPublication = yearOfPublication;
+    this.summary = summary;
+    this.thumbnail = thumbnail;
     this.info = function () {
-        const initialMsg =
-            `${this.title} by ${this.author}, ${this.pages} pages, `;
-        return read ?
-            initialMsg.concat("has been read.") :
-            initialMsg.concat("not read yet.");
+        return `${this.title} by ${this.author}, has been ${status}`;
     };
 }
+
+/**
+ * Loop through the array `myCatalog` which represents the user's books,
+ * and display each of their books on the page as a catalog card.
+ * If the book already exists on the DOM, then don't make a duplicate.
+ */
+function displayCatalog() {
+    const catalogDisplay = document.querySelector("#catalog-display");
+    const fragment = document.createDocumentFragment();
+
+    myCatalog.forEach((book) => {
+        if (bookCatalogCardExists(book)) { return; }
+
+        const div = createElementCatalogCard(book);
+        fragment.appendChild(div);
+    });
+
+    catalogDisplay.appendChild(fragment);
+}
+
+// #region - Helper functions A-Z
+/** 
+ * Notes:
+ * 1. createElement____ is a prefix AND always returns 'element'.
+ * 
+ * WARNING/FUTURE ISSUE(s): 
+ * 1. There's currently no functionality for the status to adjust color 
+ *    in the future, if the user edits a catalog card, by marking 
+ *    a book as unread / reading / completed. 
+*/
+
+/**
+ * Identify if a `<div class="catalog-card">` has been already created and
+ * placed into the DOM for a given book, by matching its title & author.
+ * (Case-insensitive matching).
+ * 
+ * @param {Book} bookObj 
+ * @return `true`, if found in DOM. Else, `false`.
+ */
+function bookCatalogCardExists(bookObj) {
+    const bookTitles = document.querySelectorAll(".book-title");
+    for (let i = 0; i < bookTitles.length; i++) {
+        const bookTitle = bookTitles[i];
+        if ((bookTitle.textContent.toLowerCase() ===
+            bookObj.title.toLowerCase()) &&
+            (bookTitle.nextElementSibling.textContent.toLowerCase() ===
+                bookObj.author.toLowerCase())) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/**
+ * Create an accessible SVG with a tooltip on hover.
+ * 
+ * @param {String} viewBox (e.g. "0 0 24 24")
+ * @param {String} ariaLabelledBy (the `<title>` id attribute for the given SVG)
+ * @param {String} href (the id attribute of `<g>` of the SVG; e.g. "#icon-abc")
+ */
+function createElementAccessibleSVG(viewBox, ariaLabelledBy, href) {
+    const element = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    element.setAttribute("viewBox", viewBox);
+    element.setAttribute("aria-labelledby", ariaLabelledBy);
+
+    const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
+    use.setAttribute("href", href);
+
+    element.appendChild(use);
+    return element;
+}
+
+/**
+ * Part of the programmatic creation of a complete `<div class="catalog-card">`.
+ * 
+ * @returns a complete `<ul class="card-options">` with all options.
+ */
+function createElementCardOptions() {
+    const optionSVGs = [
+        {
+            viewBox: "0 0 42 25",
+            ariaLabelledBy: "mark-completed",
+            href: "#icon-custom-check-all"
+        }, {
+            viewBox: "0 0 42 40",
+            ariaLabelledBy: "mark-unread",
+            href: "#icon-custom-uncheck-all"
+        }, {
+            viewBox: "0 0 24 24",
+            ariaLabelledBy: "add-to-collection",
+            href: "#icon-bookshelf"
+        }, {
+            viewBox: "0 0 24 24",
+            ariaLabelledBy: "move-to-trash",
+            href: "#icon-delete-outline"
+        }, {
+            viewBox: "0 0 24 24",
+            ariaLabelledBy: "view-full-details",
+            href: "#icon-arrow-expand"
+        }, {
+            viewBox: "0 0 24 24",
+            ariaLabelledBy: "edit-card-info",
+            href: "#icon-text-box-edit-outline"
+        }, {
+            viewBox: "0 0 24 24",
+            ariaLabelledBy: "replace-thumbnail",
+            href: "#icon-image-edit"
+        }
+    ];
+    const element = createElementWithClass("ul", "card-options");
+    for (let i = 0; i < 7; i++) {
+        const li = document.createElement("li");
+        const button = document.createElement("button");
+        const svg = createElementAccessibleSVG(
+            optionSVGs[i].viewBox,
+            optionSVGs[i].ariaLabelledBy,
+            optionSVGs[i].href
+        );
+        button.appendChild(svg);
+        li.appendChild(button);
+        element.appendChild(li);
+    }
+    return element;
+}
+
+/**
+ * Part of the programmatic creation of a complete `<div class="catalog-card">`.
+ * 
+ * @param {Book} bookObj
+ * @returns a complete `<div class="catalog-card">`.
+ */
+function createElementCatalogCard(bookObj) {
+    const element = createElementWithClass("div", "catalog-card");
+    const thumbnail = createElementThumbnail(bookObj.thumbnail);
+    const title = createElementWithClass("h3", "book-title", bookObj.title);
+    const author = createElementWithClass("p", "book-author", bookObj.author);
+    const status = createElementStatus(bookObj.status);
+    const options = createElementCardOptions();
+
+    element.append(thumbnail, title, author, status, options);
+    return element;
+}
+
+/**
+ * Part of the programmatic creation of a complete `<div class="catalog-card">`.
+ * 
+ * @param {String} bookObjStatus
+ * 
+ */
+function createElementStatus(bookObjStatus) {
+    const element = createElementWithClass("p", "book-status", bookObjStatus);
+    switch (bookObjStatus.toLowerCase()) {
+        case UNREAD:
+            element.classList.add("unread");
+            element.classList.remove("reading");
+            element.classList.remove("completed");
+            break;
+        case READING:
+            element.classList.remove("unread");
+            element.classList.add("reading");
+            element.classList.remove("completed");
+            break;
+        case COMPLETED:
+            element.classList.remove("unread");
+            element.classList.remove("reading");
+            element.classList.add("completed");
+            break;
+    }
+    return element;
+}
+
+/**
+ * Part of the programmatic creation of a complete `<div class="catalog-card">`.
+ *  
+ * @param {String} srcPath optional; (e.g. "./assets/thumbnails/xyz.abc")
+ * @param {String} altText optional; (e.g. "BookTitle's Cover")
+ * 
+ * Notes:
+ *  1. Ideal eBook cover art has a height/width ratio of 1.6:1 (e.g. 256:160).
+ *  2. Why 160x256 specifically is due to following reference design.
+ */
+function createElementThumbnail(srcPath = "./assets/thumbnails/default-book-thumbnail.svg", altText = "Thumbnail of book") {
+    const element = createElementWithClass("img", "book-thumbnail");
+    element.setAttribute("src", srcPath);
+    element.setAttribute("alt", altText);
+    element.setAttribute("width", "160");
+    element.setAttribute("height", "256");
+    return element;
+}
+
+/**
+ * Expanded functionality for `document.createElement()`.
+ * 
+ * @param {String} tag 
+ * @param {String} className 
+ * @param {String} textContent optional;
+ */
+function createElementWithClass(tag, className, textContent = "") {
+    const element = document.createElement(tag);
+    element.className = className;
+    element.textContent = textContent;
+    return element;
+}
+// #endregion
+
+displayCatalog();
